1153f81187302527fe1e45f8649fd98ba97fde2b
Rotem Harel <me@rotemharel.com>
2013-12-22 17:03:07 +0200

    Added OS selector script; some design changes.

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -4,6 +4,25 @@
 $(document).ready(function(){
   var next_url = $('#continue').attr('href');
 
+  init_navigation_keys();
+  init_os_selector();
+  init_header_shadow();
+});
+
+var body = $('body');
+
+// The header should drop shadow if the page is scrolled down.
+function init_header_shadow() {
+  $(window).scroll(function(){ 
+    if (window.scrollY > 0) {
+      body.addClass('scrolled');
+    } else {
+      body.removeClass('scrolled');
+    }
+  });
+}
+
+function init_navigation_keys() {  
   $(document).keydown(function(e){
     if (e.keyCode == 37) { 
       history.go(-1);
@@ -13,4 +32,31 @@ $(document).ready(function(){
       return false;
     }
   });
-});
+}
+
+function init_os_selector() {
+  $('#os-selector select').change(function(){
+    var os = $(this).val();
+    $('body').removeClass('os-nix').removeClass('os-windows').removeClass('os-osx').addClass('os-' + os);
+    document.cookie = "os=" + os;
+  });
+
+  var os = cookies()['os'];
+  if (typeof os == "undefined") {
+    os = 'windows';
+  }
+  $('body').addClass('os-' + os);
+  $('#os-selector select').val(os);
+}
+
+function cookies() {
+  var cookie = document.cookie.split('; '),
+      cookies = {};
+
+  for (var i = 0; i < cookie.length; i++) {
+    var cook = cookie[i].split('=');
+    cookies[cook[0]] = cook[1];
+  }
+
+  return cookies;
+}
