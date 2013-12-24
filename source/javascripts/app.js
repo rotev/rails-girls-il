085dac94783ce484759ffc232e0dc11e65ab067c
Rotem Harel <me@rotemharel.com>
2013-12-25 01:42:43 +0200

    Fixed the cookie bug (#15)

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -40,27 +40,15 @@ function init_os_selector() {
   $('#os-selector select').change(function(){
     var os = $(this).val();
     $('body').removeClass('os-nix').removeClass('os-windows').removeClass('os-osx').addClass('os-' + os);
-    document.cookie = "os=" + os;
+    $.cookie("os", os, {path: '/'});
   });
 
-  var os = cookies()['os'];
+  var os = $.cookie("os");
   if (typeof os == "undefined") {
     os = 'windows';
   }
   $('body').addClass('os-' + os);
   $('#os-selector select').val(os);
-}
-
-function cookies() {
-  var cookie = document.cookie.split('; '),
-      cookies = {};
-
-  for (var i = 0; i < cookie.length; i++) {
-    var cook = cookie[i].split('=');
-    cookies[cook[0]] = cook[1];
-  }
-
-  return cookies;
 }
 
 function init_rotating_photos() {
--- source/javascripts/foundation/index.js
+++ source/javascripts/foundation/index.js
@@ -2,7 +2,6 @@
 =require foundation/foundation
 =require foundation/foundation.alerts
 =require foundation/foundation.clearing
-=require foundation/foundation.cookie
 =require foundation/foundation.dropdown
 =require foundation/foundation.forms
 =require foundation/foundation.joyride
