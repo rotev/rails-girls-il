9b765960c9d285af327694a10cc70827ec7ce672
Ilona Shub <ilona.shub@klarna.com>
2016-01-24 17:06:24 +0200

    fix the header

--- source/javascripts/layout.js
+++ source/javascripts/layout.js
@@ -2,7 +2,8 @@ $(function() {
   var pgurl = window.location.href.substr(window.location.href
     .lastIndexOf("/"));
   $("nav ul li a").each(function(){
-    if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
+    url = $(this).attr("href").replace("/en", "");
+    if(url == pgurl || url == '' )
       $(this).addClass("active");
   })
 });
