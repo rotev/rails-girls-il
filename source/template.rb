cbecb15b927f9989981cbc2a5319115048207cdb
Inbal Gilai <inbal.gilai@klarna.com>
2016-10-27 20:28:28 +0300

    fix simple_scaffold template's routes section

--- source/template.rb
+++ source/template.rb
@@ -70,13 +70,16 @@ def generate_explicit_routes
 class Rails::ExplicitRouteGenerator < Rails::Generators::NamedBase
   def create_explicit_routes
     route "delete '/\#{plural_name}/:id'       => '\#{plural_name}\#destroy'\\n"
+
     route "put    '/\#{plural_name}/:id'       => '\#{plural_name}\#update'\\n"
     route "patch  '/\#{plural_name}/:id'       => '\#{plural_name}\#update'"
-    route "get    '/\#{plural_name}/:id/edit'  => '\#{plural_name}\#edit'"
-    route "post   '/\#{plural_name}/:id'       => '\#{plural_name}\#create'\\n"
-    route "get    '/\#{plural_name}/new'       => '\#{plural_name}\#new'"
-    route "get    '/\#{plural_name}/:id'       => '\#{plural_name}\#show'\\n"
-    route "get    '/\#{plural_name}'           => '\#{plural_name}\#index'"
+    route "post   '/\#{plural_name}'           => '\#{plural_name}\#create'"
+
+    route "get    '/\#{plural_name}/:id'       => '\#{plural_name}\#show', as: 'idea'\\n"
+    route "get    '/\#{plural_name}/:id/edit'  => '\#{plural_name}\#edit', as: 'edit_idea'"
+    route "get    '/\#{plural_name}/new'       => '\#{plural_name}\#new', as: 'new_idea'"
+
+    route "get    '/\#{plural_name}'           => '\#{plural_name}\#index'\\n"
   end
 end
   CODE
